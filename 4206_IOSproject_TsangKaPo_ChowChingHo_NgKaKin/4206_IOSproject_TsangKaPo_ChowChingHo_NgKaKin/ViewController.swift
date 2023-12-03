a1ecf5ef9e4ed2e8bd1c3e61a0bb8fbb492c463f
LeoNg <45294674+ngka@users.noreply.github.com>
2023-12-03 20:00:34 +0800

    Add files via upload

---
+++ 4206_IOSproject_TsangKaPo_ChowChingHo_NgKaKin/4206_IOSproject_TsangKaPo_ChowChingHo_NgKaKin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4206_IOSproject_TsangKaPo_ChowChingHo_NgKaKin
+//
+//  Created by NgKaKin on 3/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
