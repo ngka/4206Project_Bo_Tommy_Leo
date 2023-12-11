523b2850f6a8f994f0d3921a3f7d9654a199e9e4
Leo <junojuno5112@gmail.com>
2023-12-11 23:26:51 +0800

    change layout

---
+++ 4206_IOSproject_TsangKaPo_ChowChingHo_NgKaKin/4206_IOSproject_TsangKaPo_ChowChingHo_NgKaKin/AddEditViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AddEditViewController.swift
+//  4206_IOSproject_TsangKaPo_ChowChingHo_NgKaKin
+//
+//  Created by NgKaKin on 11/12/2023.
+//
+
+import UIKit
+
+class AddEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
